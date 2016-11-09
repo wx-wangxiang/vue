10509db413f188222a7ddeb31d4b910edf1aaff2
tonny wang <wx18157142125@163.com>
2016-11-09 23:43:20 +0800

    to-do-list

--- app/components/app.vue
+++ app/components/app.vue
@@ -1,19 +1,66 @@
 <template>
-	<div class="message">{{msg}}</div>
+	<h1 class="title">This is my to-do list</h1>
+	<input type="text" v-model="newItem" @keyup.enter="addItem">
+	<ul class="to-do-list">
+		<li v-for="item in items" v-text="item.label" @click="clickHandler(item)" :class="{hasFinished: item.finished}"></li>
+	</ul>
 </template>
 <script>
 	export default{
 		data() {
 			return {
-				msg: 'hello world!dddddd'
+				items: [{
+					label: 'eat dinner',
+					finished: false
+				},{
+					label: 'brush teeth',
+					finished: false
+				},{
+					label: 'go to bed',
+					finished: false
+				}],
+				newItem: ''
+			}
+		},
+		methods: {
+			clickHandler: function (item) {
+				item.finished = !item.finished;
+				console.log('ok');
+			},
+			addItem: function(){
+				console.log(this.newItem);
+				var item = this.newItem;
+				var obj = {
+					label: item,
+					finished: false
+				}
+				this.items.push(obj);
+				this.newItem = ''
 			}
 		}
 	}
 </script>
 <style type="text/css">
+	.title{
+		text-align: left;
+	}
+	.to-do-list li{
+		cursor: pointer;
+		-webkit-user-select: none;
+		user-select: none;
+	}
 	.message{
 		color: lightblue;
 		font-size: 36px;
 		font-weight: bold;
+	}
+	.red{
+		color: red;
+	}
+	.green{
+		color: green;
+	}
+	.hasFinished{
+		text-decoration: line-through;
 	}
 </style>
