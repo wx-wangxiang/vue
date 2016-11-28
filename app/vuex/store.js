5a9311bce7d20c5f655ce2b875c1f541b8a6daa7
wangxiang <2279334914@qq.com>
2016-11-28 13:30:29 +0800

    add random name

--- app/vuex/store.js
+++ app/vuex/store.js
@@ -18,10 +18,12 @@ const store = new Vuex.Store({
       state.msgList.push(state.newComment);
     },
     updateMsg (state, msg) {
-    	var tempComment = {
-    		author: 'wang xiang',
-    		content: msg
-    	}
+      //加了名字随机的功能，是为了实现msgCountBy这个getter
+      var name = ['wang xiang', 'ming', 'ding'][Math.floor(Math.random() * 3)],
+    	    tempComment = {
+        		author: name,
+        		content: msg
+        	}
     	state.newComment = tempComment;
     }
   },
